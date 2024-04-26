1ce6b8f5e5bf0c9c9f2cced86bc31f0ecd27ba99
aldreichh <johnhcierdla@gmail.com>
2024-04-26 16:12:03 +0800

    working virustotal api

--- public/background.js
+++ public/background.js
@@ -1,3 +1,1 @@
-console.log('hello');
-
-
+console.log('Background script executing...');
--- src/components/ProtectionHistory.js
+++ src/components/ProtectionHistory.js
@@ -1,22 +1,50 @@
 /*global chrome*/
 import React, { useState } from 'react';
+import axios from 'axios';
 
-function ProtectionHistory() {
-    const [currentURL, setCurrentURL] = useState('');
-    
-    const handleClick = () => {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const currentUrl = tabs[0].url;
-          console.log(currentUrl);
-          setCurrentURL(currentUrl);
-        });
-    };
-    return(
-        <>
-            Protection
-            <button onClick={handleClick}>Get Current URL</button>
-            {currentURL}
-        </>
-    );
-}
-export default ProtectionHistory;
+const YourComponent = () => {
+  const [api, setAPI] = useState('e606af073d0c541c38b356e1f3590364cde310c12f202bf4b731c73ab02246d8');
+  const [report, setReport] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleClick = async () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+      const currentUrl = tabs[0].url;
+      console.log('Current URL:', currentUrl);
+      const url = new URL(currentUrl);
+      const baseDomain = `${url.protocol}//${url.hostname}`;
+      console.log('Base Domain:', baseDomain);
+
+      try {
+        const response = await axios.get(`http://localhost:5000/url-report?apikey=${api}&resource=${baseDomain}&allinfo=false&scan=0`);
+        console.log(response.data);
+        setReport(response.data);
+        setError(null);
+      } catch (error) {
+        console.log(error.response.data.error);
+        setError(error.response.data.error);
+        setReport(null);
+      } 
+
+    });
+  };
+
+  const handleNotif = () => {
+    const confirmed = window.confirm('This website has been detected as malicious, do you want to add this to blocklisted URLs?');
+          if (confirmed) {
+            console.log('User clicked "OK"');
+          } else {
+            console.log('User clicked "Cancel"');
+          }
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>Send Request</button>
+      <button onClick={handleNotif}>show notification example</button>
+    </div>
+  );
+
+};
+
+export default YourComponent;
